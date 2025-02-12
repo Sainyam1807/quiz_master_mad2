f2b8802605c96d4a399aeb31fef06fe931a1ddf9
Sainyam <23f1002391@ds.study.iitm.ac.in>
2025-02-12 19:46:46 +0530

    Initialized private GitHub repository with README and .gitignore

---
+++ application/config.py
@@ -0,0 +1,16 @@
+class Config():
+    DEBUG = False   # debug is a variable
+    SQLALCHEMY_TRACK_MOADIFICATIONS = True   # variable
+
+
+class LocalDevelopmentConfig(Config):  # inheriting the config class
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = "sqlite:///quizmasterv2.sqlite3"
+    DEBUG = True   # overriding the variable DEBUG
+
+    # security configurations
+    SECRET_KEY="this-is-secretkey"  # hashes user credentials in sessions
+    SECURITY_PASSWORD_HASH="bcrypt"  # bcrypt is the mechanism for encrypting or hashing password
+    SECURITY_PASSWORD_SALT="this-is-password-salt" # helps in encrypting / hashing the password
+    WTF_CSRF_ENABLED= False  # related to the forms info that it is coming from the same application form
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = "authentication-token"
